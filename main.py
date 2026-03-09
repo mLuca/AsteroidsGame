cd47318c86e8fab39c99b74d7064b04cec4eb395
mLuca <mazzon.luca@gmail.com>
2026-03-09 12:21:32 +0100

    Added dT and fixed to 60 fps

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
 
-        #Always last call in game loop
+        #Always last calls in game loop
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
